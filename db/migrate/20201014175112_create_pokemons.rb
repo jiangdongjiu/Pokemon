596ffaeb0c41f394a2fcc65784565325506b1f1e
dongjiu jiang <jiangdongjiu@dongjius-MacBook-Pro.local>
2020-10-14 12:52:30 -0500

    scaffold pokemon model, controller and view

---
+++ db/migrate/20201014175112_create_pokemons.rb
@@ -0,0 +1,11 @@
+class CreatePokemons < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.integer :weight
+      t.integer :height
+
+      t.timestamps
+    end
+  end
+end
